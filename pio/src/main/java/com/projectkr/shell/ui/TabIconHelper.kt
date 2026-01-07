07a4030e590cc2c6b42457b2bd2536c6617ae8b7
Kakathic <kakathic@gmail.com>
2026-01-07 13:43:05 +0700

    Update TabIconHelper.kt

--- pio/src/main/java/com/projectkr/shell/ui/TabIconHelper.kt
+++ pio/src/main/java/com/projectkr/shell/ui/TabIconHelper.kt
@@ -6,7 +6,6 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
-import android.widget.TabHost
 import android.widget.TextView
 import com.projectkr.shell.R
 
