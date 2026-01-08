e97c47af3371687369030ef8555e1b410004cf0c
Kakathic <kakathic@gmail.com>
2026-01-08 16:48:29 +0700

    Update PIO.kt

--- pio/src/main/java/com/projectkr/shell/PIO.kt
+++ pio/src/main/java/com/projectkr/shell/PIO.kt
@@ -1,18 +1,5 @@
 package com.projectkr.shell
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 
-class PIO : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        // Ép ứng dụng luôn dùng DARK MODE
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-
-        // Các lựa chọn khác nếu cần:
-        // AppCompatDelegate.MODE_NIGHT_NO          // Luôn light mode
-        // AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM // Theo hệ thống (mặc định)
-        // AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY // Tự động theo tiết kiệm pin (cũ)
-    }
-}
+class PIO : Application()
