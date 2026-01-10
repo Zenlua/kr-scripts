a4b900ecac2d0a31ae5ee891be676650b2e9cead
Kakathic <kakathic@gmail.com>
2026-01-10 13:30:08 +0700

    Delete ShellOutput.kt

--- common/src/main/java/com/omarea/common/shell/ShellOutput.kt
+++
@@ -1,9 +0,0 @@
-package com.omarea.common.shell
-
-sealed class ShellOutput {
-    data class Text(val text: String) : ShellOutput()
-    data class Image(
-        val path: String,
-        val caption: String? = null
-    ) : ShellOutput()
-}
