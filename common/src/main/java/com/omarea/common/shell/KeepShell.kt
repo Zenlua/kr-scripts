5de24462610723717d9599c681a7c694630c1817
Kakathic <kakathic@gmail.com>
2026-01-10 10:30:28 +0700

    Update KeepShell.kt

--- common/src/main/java/com/omarea/common/shell/KeepShell.kt
+++ common/src/main/java/com/omarea/common/shell/KeepShell.kt
@@ -162,7 +162,6 @@ class KeepShell(private var rootMode: Boolean = true) {
 
     /** Thực thi lệnh với dịch ResourceID */
     fun doCmdSync(cmd: String, translation: ShellTranslation): String {
-        val rows = doCmdSync(cmd).split("\n")
-        return if (rows.isNotEmpty()) translation.resolveRows(rows) else ""
+    return translation.getTranslatedResult(cmd, this)
     }
 }
