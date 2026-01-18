196e44a86fff2355f201375db5b3617e748dac0c
Kakathic <kakathic@gmail.com>
2026-01-18 22:36:59 +0700

    Update ActivityFileSelector.kt

--- pio/src/main/java/com/projectkr/shell/ActivityFileSelector.kt
+++ pio/src/main/java/com/projectkr/shell/ActivityFileSelector.kt
@@ -1,8 +1,6 @@
 package com.projectkr.shell
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
 import android.view.KeyEvent
@@ -10,8 +8,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.app.ActivityCompat
-import androidx.core.content.PermissionChecker
 import com.omarea.common.ui.ProgressBarDialog
 import com.projectkr.shell.databinding.ActivityFileSelectorBinding
 import com.projectkr.shell.ui.AdapterFileSelector
@@ -73,61 +69,35 @@ class ActivityFileSelector : AppCompatActivity() {
         return super.onKeyDown(keyCode, event)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        var grant = true
-        for (result in grantResults) {
-            if (result == PackageManager.PERMISSION_DENIED) {
-                grant = false
-            }
-        }
-
-        if (requestCode == 111) {
-            if (!grant) {
-                Toast.makeText(applicationContext, "没有读取文件的权限！", Toast.LENGTH_LONG).show()
-            } else {
-                loadData()
-            }
-        }
-    }
-
-    private fun checkPermission(permission: String): Boolean = PermissionChecker.checkSelfPermission(this, permission) == PermissionChecker.PERMISSION_GRANTED
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
-    }
-
     override fun onResume() {
         super.onResume()
         loadData()
     }
 
     private fun loadData() {
-        if (checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE) && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            val sdcard = File(Environment.getExternalStorageDirectory().absolutePath)
-            if (sdcard.exists() && sdcard.isDirectory) {
-                val list = sdcard.listFiles()
-                if (list == null) {
-                    Toast.makeText(applicationContext, "获取文件列表失败！", Toast.LENGTH_LONG).show()
-                    return
-                }
-                val onSelected =  Runnable {
-                    val file: File? = adapterFileSelector!!.selectedFile
-                    if (file != null) {
-                        this.setResult(RESULT_OK, Intent().putExtra("file", file.absolutePath))
-                        this.finish()
-                    }
-                }
-                adapterFileSelector = if (mode == MODE_FOLDER) {
-                    AdapterFileSelector.FolderChooser(sdcard, onSelected, ProgressBarDialog(this))
-                } else {
-                    AdapterFileSelector.FileChooser(sdcard, onSelected, ProgressBarDialog(this), extension)
-                }
-
-                binding.fileSelectorList.adapter = adapterFileSelector
+        val sdcard = File(Environment.getExternalStorageDirectory().absolutePath)
+        if (sdcard.exists() && sdcard.isDirectory) {
+            val list = sdcard.listFiles()
+            if (list == null) {
+                Toast.makeText(applicationContext, "Failed to retrieve file list!", Toast.LENGTH_LONG).show()
+                return
             }
+            val onSelected =  Runnable {
+                val file: File? = adapterFileSelector!!.selectedFile
+                if (file != null) {
+                    this.setResult(RESULT_OK, Intent().putExtra("file", file.absolutePath))
+                    this.finish()
+                }
+            }
+            adapterFileSelector = if (mode == MODE_FOLDER) {
+                AdapterFileSelector.FolderChooser(sdcard, onSelected, ProgressBarDialog(this))
+            } else {
+                AdapterFileSelector.FileChooser(sdcard, onSelected, ProgressBarDialog(this), extension)
+            }
+
+            binding.fileSelectorList.adapter = adapterFileSelector
         } else {
-            requestPermissions()
+            Toast.makeText(applicationContext, "External storage not available!", Toast.LENGTH_LONG).show()
         }
     }
 }
