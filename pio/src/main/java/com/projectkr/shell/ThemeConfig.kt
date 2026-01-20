30712c0cdd7129d3663a51fb4a58838bd46e729d
Kakathic <kakathic@gmail.com>
2026-01-20 17:35:56 +0700

    Update ThemeConfig.kt

--- pio/src/main/java/com/projectkr/shell/ThemeConfig.kt
+++ pio/src/main/java/com/projectkr/shell/ThemeConfig.kt
@@ -4,8 +4,8 @@ import android.app.Activity
 import android.content.Context
 import androidx.core.content.edit
 
-class ThemeConfig (private val activity: Activity) {
-    private val config = activity.getSharedPreferences("kr-script-config", Context.MODE_PRIVATE)
+class ThemeConfig(private val context: Context) {
+    private val config = context.getSharedPreferences("kr-script-config", Context.MODE_PRIVATE)
 
     fun getAllowTransparentUI(): Boolean {
         return config.getBoolean("TransparentUI", false)
@@ -13,7 +13,9 @@ class ThemeConfig (private val activity: Activity) {
 
     fun setAllowTransparentUI(allow: Boolean) {
         config.edit { putBoolean("TransparentUI", allow) }
-        activity.recreate()
+        if (context is Activity) {
+            context.recreate()
+        }
     }
 
     fun getAllowNotificationUI(): Boolean {
@@ -22,12 +24,14 @@ class ThemeConfig (private val activity: Activity) {
 
     fun setAllowNotificationUI(allow: Boolean) {
         config.edit { putBoolean("NotificationUI", allow) }
-        activity.recreate()
-        val context = activity.applicationContext
+        if (context is Activity) {
+            context.recreate()
+        }
+        val appContext = context.applicationContext
         if (allow) {
-            WakeLockService.startService(context)
+            WakeLockService.startService(appContext)
         } else {
-            WakeLockService.stopService(context)
+            WakeLockService.stopService(appContext)
         }
     }
 }
