b6d04fb7e66d09c8d7c704ce909e037d1a48ea13
Kakathic <kakathic@gmail.com>
2026-01-20 17:37:20 +0700

    Update WakeLockService.kt

--- pio/src/main/java/com/projectkr/shell/WakeLockService.kt
+++ pio/src/main/java/com/projectkr/shell/WakeLockService.kt
@@ -52,7 +52,8 @@ class WakeLockService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        if (ThemeConfig(activity).getAllowNotificationUI()) {
+        val themeConfig = ThemeConfig(applicationContext)
+        if (themeConfig.getAllowNotificationUI()) {
             startForeground(1, buildNotification())
         }
     }
